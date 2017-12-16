b004228d7c0b756cfa146b9e63e2b3e93bbd5d97
sendersson <william@skoglycke.se>
2017-12-16 01:53:23 +0100

    Mk. 4

--- app/src/main/java/se/skoglycke/minted/MainActivity.java
+++ app/src/main/java/se/skoglycke/minted/MainActivity.java
@@ -1,7 +1,13 @@
 package se.skoglycke.minted;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sound_button;
+        sound_button = (Button) findViewById(R.id.sound_button);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.pickle_rick);
+
+        sound_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
     }
 }
